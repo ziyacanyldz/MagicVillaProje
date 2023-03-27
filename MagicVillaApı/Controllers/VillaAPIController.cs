38c2825a41324911ea74fa6b337d3881bf7f621d
ziyacanyldz <96024765+ziyacanyldz@users.noreply.github.com>
2023-03-27 21:18:59 +0300

    Cleanup and First endpoint

---
+++ MagicVillaApı/Controllers/VillaAPIController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVillaApı.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaApı.Controllers
+{
+    [ApiController]
+    [Route("api/VillaApi")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+            new Villa{Id=1,Name="Pool View" },
+            new Villa{Id=2,Name="Beach View" }
+            };
+        }
+    }
+}
---
+++ MagicVillaApı/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVillaApı.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
